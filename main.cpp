e5b1241f93924e1dfac3d67187094ae820a03c3e
houssainy <mohamed.houssainy@hotmail.com>
2013-11-26 08:28:48 +0200

    all methods done initially

--- main.cpp
+++ main.cpp
@@ -8,17 +8,24 @@ using namespace std;
 */
 struct station{
     int waiting_passengers;
-    pthread_mutex_t load_train;
+    pthread_mutex_t passenger_lock;
+    pthread_mutex_t train_lock;
 };
 
+/*Header*/
+void station_init(struct station *station);
+void station_on_board(struct station *station);
+void station_load_train(struct station *station, int count);
+void station_wait_for_train(struct station *station);
+
 /**
-* Initilization of station
+* Initilization of station.
 */
 void station_init(struct station *station)
 {
-	station.waiting_passengers = 0;
+	station->waiting_passengers = 0;
 
-	if(pthread_mutex_init(&load_train, NULL) != 0 ){
+	if(pthread_mutex_init(&station->passenger_lock, NULL) != 0 && pthread_mutex_init(&station->train_lock, NULL) != 0){
         // Error in inititilization of mutex
         cout << "Caution! Error!" << endl;
         cout << "This station has been Destroyed." << endl;
@@ -30,30 +37,37 @@ void station_init(struct station *station)
 * You must fill the train by passengers before leave this method
 */
 void station_load_train(struct station *station, int count){
-    if( count == 0 || station.waiting_passengers == 0) // No seats available, or no waiting passengers
+    if( count == 0 || station->waiting_passengers == 0) // No seats available, or no waiting passengers
         return;
 
     // There are available seats and there are waiting passengers
+    pthread_mutex_unlock(&(station->train_lock)); // lock to
     do{
         count--;
         station_on_board(station);
-    }while( count > 0 && station.waiting_passengers > 0 );
+    }while( count > 0 && station->waiting_passengers > 0 );
 
     /*No passengers left or No seats left*/
 }
 
 /**
-*
+*   New passenger has been arrived.
 */
 void station_wait_for_train(struct station *station){
-
+    pthread_mutex_lock(&(station->passenger_lock));
+    station->waiting_passengers++;
+    pthread_mutex_unlock(&(station->passenger_lock));
 }
 
 /**
-*
+*   Release one passenger from the station.
 */
 void station_on_board(struct station *station){
-
+    if( station->waiting_passengers <= 0 ) // No waiting passenger
+        return;
+    pthread_mutex_lock(&(station->passenger_lock));
+    station->waiting_passengers--;
+    pthread_mutex_unlock(&(station->passenger_lock));
 }
 
 int main(){
